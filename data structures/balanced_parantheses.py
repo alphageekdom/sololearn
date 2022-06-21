b53448b0d554ef5c1ac3b5425adec3a138a41b59
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-06-21 16:20:06 -0700

    Update balanced_parantheses.py

--- data structures/balanced_parantheses.py
+++ data structures/balanced_parantheses.py
@@ -1,4 +1,15 @@
 def balanced(expression):
-    pass
+    stack = []
+    
+    for char in expression:
+        if char == "(":
+            stack.insert(0, char)
+        if char == ")":
+            if "(" in stack:
+                stack.pop()
+            else:
+                return False
+    
+    return False if stack else True
 
 print(balanced(input()))
