33572de99277ec785130db083d9e942def9f3b3a
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-06-22 10:42:04 -0700

    new probs

---
+++ intermediate/car_data_dict.py
@@ -0,0 +1,8 @@
+car = {
+    'brand': 'BMW',
+    'year': 2018,
+    'color': 'red',
+}
+
+a = input()
+print(car[a])
---
+++ intermediate/qualified_sets.py
@@ -0,0 +1,2 @@
+skills = {'Python', 'HTML', 'SQL', 'C++', 'Java', 'Scala'}
+job_skills = {'HTML', 'CSS', 'JS', 'C#', 'NodeJS'}
