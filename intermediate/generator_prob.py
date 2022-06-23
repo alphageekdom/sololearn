6ff5be5bf36644f987e49de7b53bad2130166691
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-06-23 15:45:05 -0700

    new

---
+++ intermediate/generator_prob.py
@@ -0,0 +1,17 @@
+# Example 1
+def countdown():
+    i = 5
+    while i > 0:
+        yield i
+        i -= 1
+
+for i in countdown():
+    print(i)
+
+# Example 2
+def numbers(x):
+    for i in range(x):
+        if i % 2 == 0:
+            yield i
+
+print(list(numbers(11)))
