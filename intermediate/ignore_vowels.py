1ec3434fc3ffcc21f39c0f154045d53f47dccbf3
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-06-23 14:13:13 -0700

    new probs/update

---
+++ intermediate/ignore_vowels.py
@@ -0,0 +1,3 @@
+word = input()
+res = [char for char in word if char not in "aeiou"]
+print(res)
---
+++ intermediate/letter_counter.py
@@ -0,0 +1,6 @@
+text = input()
+dict = {}
+
+for char in text:
+    dict[char] = text.count(char)
+print(dict)
