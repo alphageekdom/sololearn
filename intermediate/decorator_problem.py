9dc0c85aa97247b9a4564dfc7c84bf98aeebea03
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-06-27 09:52:41 -0700

    update

---
+++ intermediate/decorator_problem.py
@@ -0,0 +1,15 @@
+def decor(func):
+    def wrap(num):
+        print("***")
+        func(num)
+        print("***")
+        print("END OF PAGE")
+    return wrap
+
+
+@decor
+def invoice(num):
+    print("INVOICE #" + num)
+
+
+invoice(input())
