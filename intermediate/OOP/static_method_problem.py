9ac3513139411d59abb2f9f6b01db3f28e722849
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-06-28 17:21:14 -0700

    last of the OOP problems

---
+++ intermediate/OOP/static_method_problem.py
@@ -0,0 +1,15 @@
+class Shape:
+
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    @staticmethod
+    def area(w, h):
+        return w * h
+
+
+w = int(input())
+h = int(input())
+
+print(Shape.area(w, h))
